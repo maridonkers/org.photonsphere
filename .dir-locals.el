a4a262b855ed10d1896d31cf261e23a29ccb1f34
A.H.M. (Mari) Donkers <a.h.m.donkers@photonsphere.org>
2023-07-29 22:35:29 +0200

    Deleted files first

--- .dir-locals.el
+++
@@ -1,9 +0,0 @@
-((nil
-  (eval . (dante-mode 0))
-  (eval . (flycheck-mode 0))
-  (dante-repl-command-line . ("ghci"))
-  (haskell-process-type . ghci)
-  (format-all-formatters
-   ("Haskell" ormolu)
-   ("Literate Haskell" ormolu)
-   )))
